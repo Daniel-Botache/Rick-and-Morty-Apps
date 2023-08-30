b254a2a6f19597121720dbb20ec9d217a1521897
Daniel-Botache <daniel.botache@gmail.com>
2023-08-30 16:25:41 -0500

    commit final

--- Client /src/redux/store.js
+++ Client /src/redux/store.js
@@ -5,7 +5,7 @@ import ThunkMiddleware from "redux-thunk";
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
-  composeEnhancer(applyMiddleware(ThunkMiddleware))
+  composeEnhancer(applyMiddleware(ThunkMiddleware)) //para manejar asincronia
 );
 
 export default store;
